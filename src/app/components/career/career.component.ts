745fe6522aa97abcbc750af70c75f38023e64e64
soumyaranjan704 <57033716+soumyaranjan704@users.noreply.github.com>
2022-11-21 16:15:34 +0530

    blob added

--- src/app/components/career/career.component.ts
+++ src/app/components/career/career.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UtilityService } from 'src/app/services/utility.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-career',
@@ -6,11 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./career.component.scss']
 })
 export class CareerComponent implements OnInit {
+  public carrerGetData :any[]=[];
 
-  constructor() { }
+  constructor(private utilityService: UtilityService) { }
 
   ngOnInit(): void {
-    
+    this.getDataFromApi();
   }
+  getDataFromApi() {
+    let url = environment.getPostJob;
+    this.utilityService.getDataToService(url).subscribe(response => {
+      this.carrerGetData = response;
+      console.log('response from api is', response);
+    }, error => {
+      console.log('error is', error);
+    });
 
+
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,9 @@ export const environment = {
   production: false,
   signUpBaseUrl:'http://localhost:8080/signup',
   contactUsBaseUrl:'http://localhost:8080/contact',
-  getNewsLetter:'http://localhost:8080/newsletter'
+  getNewsLetter:'http://localhost:8080/newsletter',
+  getPostJob:'http://localhost:8080/postJobData',
+
 
 };
 
